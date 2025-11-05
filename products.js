87066481d3c7647c8a1bfc89178c285346d3d606
wacelalorshe <wacelalorshe@gmail.com>
2025-11-05 15:18:24 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,24 @@
+// بيانات المنتجات الافتراضية
+let products = [
+    {
+        id: 1,
+        name: "هاتف ذكي",
+        description: "هاتف ذكي حديث بمواصفات عالية",
+        price: 299.99,
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        id: 2,
+        name: "لابتوب",
+        description: "لابتوب قوي للأعمال والألعاب",
+        price: 899.99,
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        id: 3,
+        name: "سماعات",
+        description: "سماعات لاسلكية عالية الجودة",
+        price: 149.99,
+        image: "https://via.placeholder.com/200"
+    }
+];
